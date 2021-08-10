66b1e6536108a2dc573dcfeaa61887ec25af9d7e
kmwamasali <kmwamasali@gmail.com>
2021-08-10 16:59:54 +0300

    feat: add utility functions

---
+++ src/utilities/numbers.ts
@@ -0,0 +1,32 @@
+// multiply
+const multiply = (num1: number, num2: number): number => {
+  return num1 * num2;
+}
+
+// add
+const sum = (num1: number, num2: number): number => {
+  return num1 + num2;
+}
+
+// divide
+const divide = (num1: number, num2: number): number => {
+  return num1 / num2;
+}
+
+// subtract
+const subtract = (num1: number, num2: number): number => {
+  return num1 - num2;
+}
+
+// square
+const square = (num1: number, num2: number): number => {
+  return num1 * num2;
+}
+
+export default {
+  multiply,
+  sum,
+  divide,
+  subtract,
+  square
+}
